8657cad049a3d5dad4c4175d228b2111bf9229fd
Raphael Eça <raphael2904@gmail.com>
2024-03-21 22:09:06 -0300

    Problem 0

--- Problem 0/guess.py
+++ Problem 0/guess.py
@@ -1,6 +1,14 @@
 def get_guess():
-    guess = 10
+    guess = int(input("Enter a guess: "))
     return guess
 
 
-print(get_guess())
+def main():
+    guess = get_guess()
+    if guess == 50:
+        print("correct")
+    else:
+        print("Incorrect ")
+
+
+main()
